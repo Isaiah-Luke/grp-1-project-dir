28f2474fe2d8a3c45423dbc31543396d95c64573
Isaiah luke <isaiah.luke.08@gmail.com>
2025-11-14 22:55:26 +0800

    flask setup

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template 
+import os 
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+if __name__ == "__main__":
+    port = int(os.getenv('PORT', 5055))
+    set 
+    app.run(debug= True, port=port)
